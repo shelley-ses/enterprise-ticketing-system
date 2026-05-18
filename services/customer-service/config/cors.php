628689eeb70db04cd91877e04438348167f4c0d0
jsmkll <jasminemikaellaganinion@iskolarngbayan.pup.edu.ph>
2026-05-19 03:30:15 +0800

    fix: ticket service

--- services/customer-service/config/cors.php
+++ services/customer-service/config/cors.php
@@ -21,9 +21,11 @@ return [
 
     'allowed_origins' => [
         'http://localhost:3000',
+        'http://localhost:5176',
         'http://localhost:5173',
         'http://127.0.0.1:3000',
-        'http://127.0.0.1:5173',
+        'http://127.0.0.1:5176',
+
     ],
 
     'allowed_origins_patterns' => [],
--- services/ticket-service/app/Models/Employee.php
+++ services/ticket-service/app/Models/Employee.php
@@ -4,4 +4,6 @@ namespace App\Models;
 
 class Employee extends User
 {
+    protected $table = 'employees';
+    protected $primaryKey = 'emp_id';
 }
